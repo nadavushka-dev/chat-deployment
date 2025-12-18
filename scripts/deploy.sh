deceb9ec72ea9743803e021c8646cd1df717516d
nadavushka-dev <nadavushkadev@gmail.com>
2025-12-18 12:35:22 +0200

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ git pull origin master
 # Pull latest images from GitHub Container Registry
 docker compose pull
 # Migrate db schema changes
-docker compose run api prisma migrate deploy
+docker compose run api npx prisma migrate deploy
 # Start/restart containers with new images
 docker compose up -d
-./health-checker.sh
+./script/health-checker.sh
